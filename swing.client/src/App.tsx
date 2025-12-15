6f743520e6f1b5ceab15d1190af0ae428056daca
filip patrman <filip.patrman.023@pslib.cz>
2025-12-15 09:41:56 +0100

    pokus o nacitani

--- swing.client/src/App.tsx
+++ swing.client/src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import "./css/main.css";
 import HomePage from "./pages/HomePage";
 import LogInPage from "./pages/LogInPage";
+import MinesDimonds from "./pages/MinesDimonds";
 function App() {
   return (
     <Routes>
-        <Route path="#" element={<LogInPage/>} />
-        <Route path="/" element={<HomePage/>} />
+      <Route path="#" element={<LogInPage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/mines" element={<MinesDimonds />} />
     </Routes>
   );
 }
--- swing.client/src/pages/MinesDimonds.tsx
+++ swing.client/src/pages/MinesDimonds.tsx
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
 
+interface Coordinates {
+  x: number;
+  y: number;
+}
+
 interface Tile {
-    id: number,
-    status: "hidden" | "mine" | "diamond"
+  id: number;
+  status: "mine" | "diamond";
+  hidden: boolean;
 }
 
 const MinesDimonds = () => {
   const [minesCount, setMinesCount] = useState(2);
   const [betAmount, setBetAmount] = useState(100);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [grid, setGrid] = useState(
-    Array.from({ length: 25 }, (_, i) => ({ id: i, status: "hidden" }))
+  const [grid, setGrid] = useState<Tile[]>(
+    Array.from({ length: 25 }, (_, i) => ({
+      id: i,
+      status: "diamond",
+      hidden: true,
+    }))
   );
-  const handleTileClick = () =>{
-    
-  }
+
+  const startGame = async () => {
+      const response = await fetch(`https://localhost:54657/api/MinePattern/generate/${minesCount}`);
+     
+      if (!response.ok) {
+        throw new Error("Chyba při komunikaci se serverem");
+      }
+      {console.log(JSON.stringify(response))}
+      const mineCoordinates: Coordinates[] = await response.json();
+  };
+  
+
+  const handleTileClick = () => {};
   return (
     <div className="grid-container">
       {grid.map((tile, index) => (
