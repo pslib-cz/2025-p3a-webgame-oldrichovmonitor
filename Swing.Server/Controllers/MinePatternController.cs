30e7a9519967b3de857eb1c016540ffb094e0b1b
filip patrman <filip.patrman.023@pslib.cz>
2025-12-16 20:05:51 +0100

    tile handle click

--- Swing.Server/Controllers/MinePatternController.cs
+++ Swing.Server/Controllers/MinePatternController.cs
@@ -14,11 +14,24 @@ namespace Swing.Server.Controllers
             _minePattern = minePattern;
         }
         [HttpGet("StartGame/{mines}")]
+        /*
         public ActionResult<GridCoordinates[]> generateMines(int mines)
         {
             int diamonds = 25 - mines;
             GridCoordinates[] gridCoordinates = _minePattern.placement(mines, diamonds);
             return Ok(gridCoordinates);
         }
+        */
+        public ActionResult<int[]> GetRandomMines(int mines)
+        {
+            var random = new Random();
+            var indices = new HashSet<int>();
+            while (indices.Count < mines)
+            {
+                indices.Add(random.Next(1, 26));
+            }
+            return Ok(indices.ToArray());
+        }
+
     }
 }
--- swing.client/src/pages/MinesDimonds.tsx
+++ swing.client/src/pages/MinesDimonds.tsx
@@ -15,6 +15,7 @@ const MinesDimonds = () => {
   const [minesCount, setMinesCount] = useState(2);
   const [betAmount, setBetAmount] = useState(100);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [mineIndices, setMineIndices] = useState<number[]>([])
   const [grid, setGrid] = useState<Tile[]>(
     Array.from({ length: 25 }, (_, i) => ({
       id: i,
@@ -25,16 +26,21 @@ const MinesDimonds = () => {
 
   const startGame = async () => {
       const response = await fetch(`/api/MinePattern/StartGame/${minesCount}`);
-      const data = await response.json();
+      const data: number[] = await response.json();
+      setMineIndices(data)
       console.log(data);
   };
 
-  const handleTileClick = () => {};
+  const handleTileClick = (index: number) => {
+    if(mineIndices.includes(index)){
+      alert("Bomba mrdko")
+    }else {alert("Diamand")}
+  };
   return (
     <>
       <div className="grid-container">
         {grid.map((tile, index) => (
-          <div key={tile.id} onClick={handleTileClick} className=""></div>
+          <div key={tile.id} onClick={()=> handleTileClick(index)} ></div>
         ))}
       </div>
       <button onClick={()=> startGame()}>bet</button>
